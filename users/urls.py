0951482e67be06bc57b524eb264cdd5d20972930
pulgaresking <newbiepaul24@gmail.com>
2020-08-06 20:31:54 +0800

    login and logout function added

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='Register'),
+    path('register/', views.register, name='Register'),
+    path('profile/', views.profile, name='Profile'),
 ]
